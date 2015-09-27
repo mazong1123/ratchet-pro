7cca96edc95603ae4cf853d600452efcfae34de1
mazong1123 <mazong1123@gmail.com>
2015-09-27 22:56:51 +0800

    Introduced page loader.

--- js/common.js
+++ js/common.js
@@ -1,57 +1,67 @@
-/* ========================================================================
- * Ratchet: common.js v2.0.2
- * http://goratchet.com/
- * ========================================================================
- * Copyright 2015 Connor Sears
- * Licensed under MIT (https://github.com/twbs/ratchet/blob/master/LICENSE)
- * ======================================================================== */
+/* ===================================================================================
+ * RatchetPro: common.js v1.0.0
+ * https://github.com/mazong1123/ratchet-pro
+ * ===================================================================================
+ * Copyright 2015 Jim Ma
+ * Licensed under MIT (https://github.com/mazong1123/ratchet-pro/blob/master/LICENSE)
+ * Originally from https://github.com/twbs/ratchet
+ * =================================================================================== */
 
 !(function () {
-  'use strict';
+    'use strict';
 
-  // Compatible With CustomEvent
-  if (!window.CustomEvent) {
-    window.CustomEvent = function (type, config) {
-      var e = document.createEvent('CustomEvent');
-      e.initCustomEvent(type, config.bubbles, config.cancelable, config.detail);
-      return e;
-    };
-  }
-
-  // Create Ratchet namespace
-  if (typeof window.RATCHET === 'undefined') {
-    window.RATCHET = {};
-  }
-
-  // Original script from http://davidwalsh.name/vendor-prefix
-  window.RATCHET.getBrowserCapabilities = (function () {
-    var styles = window.getComputedStyle(document.documentElement, '');
-    var pre = (Array.prototype.slice
-        .call(styles)
-        .join('')
-        .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
-      )[1];
-    return {
-      prefix: '-' + pre + '-',
-      transform: pre[0].toUpperCase() + pre.substr(1) + 'Transform'
-    };
-  })();
-
-  window.RATCHET.getTransitionEnd = (function () {
-    var el = document.createElement('ratchet');
-    var transEndEventNames = {
-      WebkitTransition : 'webkitTransitionEnd',
-      MozTransition : 'transitionend',
-      OTransition : 'oTransitionEnd otransitionend',
-      transition : 'transitionend'
-    };
-
-    for (var name in transEndEventNames) {
-      if (el.style[name] !== undefined) {
-        return transEndEventNames[name];
-      }
+    // Compatible With CustomEvent
+    if (!window.CustomEvent) {
+        window.CustomEvent = function (type, config) {
+            var e = document.createEvent('CustomEvent');
+            e.initCustomEvent(type, config.bubbles, config.cancelable, config.detail);
+            return e;
+        };
     }
 
-    return transEndEventNames.transition;
-  })();
+    // Create Ratchet namespace
+    if (typeof window.RATCHET === 'undefined') {
+        window.RATCHET = {};
+    }
+
+    // Original script from http://davidwalsh.name/vendor-prefix
+    window.RATCHET.getBrowserCapabilities = (function () {
+        var styles = window.getComputedStyle(document.documentElement, '');
+        var pre = (Array.prototype.slice
+            .call(styles)
+            .join('')
+            .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
+          )[1];
+        return {
+            prefix: '-' + pre + '-',
+            transform: pre[0].toUpperCase() + pre.substr(1) + 'Transform'
+        };
+    })();
+
+    window.RATCHET.getTransitionEnd = (function () {
+        var el = document.createElement('ratchet');
+        var transEndEventNames = {
+            WebkitTransition: 'webkitTransitionEnd',
+            MozTransition: 'transitionend',
+            OTransition: 'oTransitionEnd otransitionend',
+            transition: 'transitionend'
+        };
+
+        for (var name in transEndEventNames) {
+            if (el.style[name] !== undefined) {
+                return transEndEventNames[name];
+            }
+        }
+
+        return transEndEventNames.transition;
+    })();
+
+    // Default page loader settings. Used by pageloader.js.
+    window.RATCHET.pageLoaderSettings = {
+        pageContentElementSelector: '.content',
+        pageNameElementAttributeName: 'data-page',
+        pageEntryScriptPath: 'scripts',
+        pageEntryScriptPrefix: 'app-',
+        pageContentReadyEventSuffix: 'ContentReady'
+    };
 }());
---
+++ js/pageloader.js
@@ -0,0 +1,35 @@
+﻿/* ===================================================================================
+ * RatchetPro: pageloader.js v1.0.0
+ * https://github.com/mazong1123/ratchet-pro
+ * ===================================================================================
+ * Copyright 2015 Jim Ma
+ * Licensed under MIT (https://github.com/mazong1123/ratchet-pro/blob/master/LICENSE)
+ * =================================================================================== */
+
+!(function () {
+    'use strict';
+
+    var checkPage = function () {
+        var pageLoaderSettings = window.RATCHET.pageLoaderSettings;
+
+        var pageContentElement = $(pageLoaderSettings.pageContentElementSelector);
+        var pageName = pageContentElement.attr(pageLoaderSettings.pageNameElementAttributeName);
+
+        if (pageName) {
+            // Load page entry script.
+            var entryScriptPath = pageLoaderSettings.pageEntryScriptPath + '/' + pageEntryScriptPrefix + pageName + '.js';
+            $.getScript(entryScriptPath)
+            .done(function (script, textStatus) {
+                // Fire page content ready event.
+                $(document).trigger(pageName + pageLoaderSettings.pageContentReadyEventSuffix);
+            })
+            .fail(function (jqxhr, statusText, errorThrown) {
+                console.log(statusText);
+                console.log(jqxhr);
+            });
+        }
+    };
+
+    // Inject checkPage() after push event fired.
+    window.addEventListener('push', checkPage);
+}());
