ab367580aa348b824471515ccdc78e0eb5bdf698
mazong1123 <mazong1123@gmail.com>
2015-10-03 14:00:18 +0800

    Added missing file

---
+++ js/modal.js
@@ -0,0 +1,46 @@
+/* ===================================================================================
+ * RatchetPro: modal.js v1.0.0
+ * https://github.com/mazong1123/ratchet-pro
+ * ===================================================================================
+ * Copyright 2015 Jim Ma
+ * Licensed under MIT (https://github.com/mazong1123/ratchet-pro/blob/master/LICENSE)
+ * Originally from https://github.com/twbs/ratchet
+ * =================================================================================== */
+
+(function () {
+    'use strict';
+
+    var eventModalOpen = new CustomEvent('modalOpen', {
+        bubbles: true,
+        cancelable: true
+    });
+
+    var eventModalClose = new CustomEvent('modalClose', {
+        bubbles: true,
+        cancelable: true
+    });
+
+    window.RATCHET.Class.Modal = window.RATCHET.Class.Component.extend({
+        init: function (componentToggle, component) {
+            var self = this;
+
+            self._super(componentToggle, component);
+        },
+
+        onComponentToggleTouchEnd: function (event) {
+            var self = this;
+            self._super(event);
+
+            if (self.component && self.component.classList.contains('modal')) {
+                var eventToDispatch = eventModalOpen;
+                if (self.component.classList.contains('active')) {
+                    eventToDispatch = eventModalClose;
+                }
+                self.component.dispatchEvent(eventToDispatch);
+                self.component.classList.toggle('active');
+
+                event.preventDefault(); // prevents rewriting url (apps can still use hash values in url)
+            }
+        }
+    });
+})();
