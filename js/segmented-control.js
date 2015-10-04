f74e917a548a35be935202564b61300c94ddd8fe
mazong1123 <mazong1123@gmail.com>
2015-10-04 11:16:36 +0800

    Added missing file.

---
+++ js/segmented-control.js
@@ -0,0 +1,96 @@
+/* ===================================================================================
+ * RatchetPro: segmented-control.js v1.0.0
+ * https://github.com/mazong1123/ratchet-pro
+ * ===================================================================================
+ * Copyright 2015 Jim Ma
+ * Licensed under MIT (https://github.com/mazong1123/ratchet-pro/blob/master/LICENSE)
+ * Originally from https://github.com/twbs/ratchet
+ * =================================================================================== */
+
+(function () {
+    'use strict';
+
+    window.RATCHET.Class.SegmentedControl = window.RATCHET.Class.Component.extend({
+        init: function (componentToggle, component) {
+            var self = this;
+
+            self.componentToggle = componentToggle;
+            self.component = component;
+
+            // Find all control items.
+            self.controlItems = self.component.querySelectorAll('.control-item');
+
+            self.initEvents();
+        },
+
+        initEvents: function(){
+            var self = this;
+
+            self.componentToggleTouchEnd = function (event) {
+                self.onComponentToggleTouchEnd(event);
+            };
+
+            var controlItemLength = self.controlItems.length;
+
+            for (var i = 0; i < controlItemLength; i++) {
+                var ci = self.controlItems[i];
+
+                ci.addEventListener('touchend', self.componentToggleTouchEnd);
+            }
+        },
+
+        dispose: function () {
+            var self = this;
+            var controlItemLength = self.controlItems.length;
+
+            for (var i = 0; i < controlItemLength; i++) {
+                var ci = self.controlItems[i];
+
+                ci.removeEventListener('touchend', self.componentToggleTouchEnd);
+            }
+        },
+
+        onComponentToggleTouchEnd: function (event) {
+            var self = this;
+            self._super(event);
+
+            var targetTab = event.target;
+
+            if (targetTab && targetTab.classList.contains('control-item')) {
+                var activeTab;
+                var activeBodies;
+                var targetBody;
+                var className = 'active';
+                var classSelector = '.' + className;
+
+                activeTab = targetTab.parentNode.querySelector(classSelector);
+
+                if (activeTab) {
+                    activeTab.classList.remove(className);
+                }
+
+                targetTab.classList.add(className);
+
+                if (!targetTab.hash) {
+                    return;
+                }
+
+                targetBody = document.querySelector(targetTab.hash);
+
+                if (!targetBody) {
+                    return;
+                }
+
+                activeBodies = targetBody.parentNode.querySelectorAll(classSelector);
+
+                for (var i = 0; i < activeBodies.length; i++) {
+                    activeBodies[i].classList.remove(className);
+                }
+
+                targetBody.classList.add(className);
+
+                event.preventDefault(); // prevents rewriting url (apps can still use hash values in url)
+            }
+        }
+    });
+})();
